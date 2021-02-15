27762d9102189d2d8907339c7d12b09836b369e6
Federico Medina <38965532+FredeHG@users.noreply.github.com>
2021-02-15 00:53:33 -0300

    Actualizo la version de springfox

--- src/main/java/meli/mutant/SwaggerConfig.java
+++ src/main/java/meli/mutant/SwaggerConfig.java
@@ -1,13 +1,20 @@
 package meli.mutant;
 
 import com.google.common.base.Predicate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collection;
+import java.util.Collections;
 
 import static com.google.common.base.Predicates.or;
 import static springfox.documentation.builders.PathSelectors.regex;
@@ -15,26 +22,25 @@ import static springfox.documentation.builders.PathSelectors.regex;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
+    @Value ("${app.version}")
+    private String version;
     @Bean
-    public Docket postsApi() {
-        return new Docket(DocumentationType.SWAGGER_2).groupName("Mutant analyzer")
-                .useDefaultResponseMessages(false)
-                .apiInfo(apiInfo())
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("Mutant")
                 .select()
-                .paths(postPaths())
-                .build();
-    }
-
-    private Predicate<String> postPaths() {
-        return or(regex("/api/posts.*"), regex("/api/v1/.*"));
+                .apis(RequestHandlerSelectors.basePackage("meli.mutant"))
+                .paths(PathSelectors.regex("/api/v1/.*"))
+                .build()
+                .apiInfo(apiInfo())
+                ;
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("Mutant analyzer")
+        return new ApiInfoBuilder()
+                .title("Mutant analyzer")
                 .description("API reference for developers")
-                .version("0.0.1")
-                .license("federicom503@gmail.com")
+                .version(version)
+                .contact(new Contact("Federico Medina", "", "federicom503@gmail.com"))
                 .build();
     }
 
